9cd7d3997e69f08f7217e6356c82393af5136f42
Yixian <puyixian@gmail.com>
2017-11-15 13:49:41 -0600

    Add log_in API, update sign_up API

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var sign_up = require('./routes/sign_up');
+var log_in = require('./routes/log_in');
 
 var app = express();
 
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/sign_up', sign_up);
+app.use('/log_in', log_in);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
