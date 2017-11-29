119e563c1ca83134b5d047c8e582f237127e7d4d
Yixian <puyixian@gmail.com>
2017-11-29 13:24:16 -0600

    Change social network links in Footer to flex layout

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
 
 import './Footer.css';
 
@@ -7,36 +6,30 @@ import facebook from './images/facebook.png';
 import google from './images/google.png';
 import instagram from './images/instagram.png';
 import youtube from './images/youtube.png';
-import linkedin from './images/linkedin.png';
+import linkedIn from './images/linkedin.png';
 
 const Footer = () => {
+  const links = [
+    { image: facebook, link: 'http://www.facebook.com' },
+    { image: google, link: 'http://www.google.com' },
+    { image: instagram, link: 'http://www.instagram.com' },
+    { image: youtube, link: 'http://www.youtube.com' },
+    { image: linkedIn, link: 'http://www.linkedIn.com' }
+  ];
+
   return (
     <div className="footer">
-      <div className="container">
-        <Row>
-          <Col xs={4} xsOffset={4} sm={4} smOffset={8}>
-            <a href="/">
-              <img src={facebook} alt="facebook link" />
+      <div className="container" id="social-network-links">
+        {links.map(linkItem => {
+          return (
+            <a href={linkItem.link} key={linkItem.link}>
+              <img src={linkItem.image} alt="social network link" />
             </a>
-            <a href="/">
-              <img src={google} alt="google plus link" />
-            </a>
-            <a href="/">
-              <img src={instagram} alt="instagram link" />
-            </a>
-            <a href="/">
-              <img src={youtube} alt="youtube link" />
-            </a>
-            <a href="/">
-              <img src={linkedin} alt="linkedin link" />
-            </a>
-          </Col>
-        </Row>
-        <Row>
-          <hr />
-          <p>Copyright &copy;2017 All Rights Reserved</p>
-        </Row>
+          );
+        })}
       </div>
+      <hr />
+      <p>Copyright &copy;2017 All Rights Reserved</p>
     </div>
   );
 };
