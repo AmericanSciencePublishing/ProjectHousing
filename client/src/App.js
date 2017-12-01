64d228e7fe810bde5833bf546033e0e39a63d37a
Yixian <puyixian@gmail.com>
2017-12-01 16:49:23 -0600

    Solve rebase conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem,  } from 'react-bootstrap';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 //import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import { Router, Route, NavLink } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -17,70 +17,68 @@ import Details from './Details';
 import AccountButton from './AccountButtonHomePage';
 import logo from './images/logo.png';
 import './App.css';
-var axios = require("axios");
+var axios = require('axios');
 
 class App extends Component {
-<<<<<<< HEAD
-    constructor() {
-	super();
-	this.state = { showModal: false,
-		       user:{},
-		       action:''
-		     };
-	this.handleClick = this.handleClick.bind(this);
-	this.close = this.close.bind(this);
-	this.sendUserToHome = this.sendUserToHome.bind(this);
-	this.isEmpty = this.isEmpty.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      showModal: false,
+      user: {},
+      action: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.close = this.close.bind(this);
+    this.sendUserToHome = this.sendUserToHome.bind(this);
+    this.isEmpty = this.isEmpty.bind(this);
+  }
 
-    componentWillMount(){
-	console.log('heiheihei');
-	console.log(this.state.user);
+  componentWillMount() {
+    console.log('heiheihei');
+    console.log(this.state.user);
 
-	//another higher level check is to write a function of isAuth()
-	//when user click 'My Profile', use onEnter{isAuth()} to check if is authed
-	//only login and logout could call isAuth()
+    //another higher level check is to write a function of isAuth()
+    //when user click 'My Profile', use onEnter{isAuth()} to check if is authed
+    //only login and logout could call isAuth()
 
-	//Right now, only sign out could set userState to 'offline', so user could
-	//aotumatically login if he never sign out.
+    //Right now, only sign out could set userState to 'offline', so user could
+    //aotumatically login if he never sign out.
 
-	axios.get('/checkUser/').then (res=>{
-	    console.log(res);
-	    if(res.data === ""){
-		console.log('no such session logged in');
-	    }
-	    else{
-		this.setState({
-		    user: res.data
-		});
-	    }
-	});
-	console.log(this.state.user);
-    }
+    axios.get('/checkUser/').then(res => {
+      console.log(res);
+      if (res.data === '') {
+        console.log('no such session logged in');
+      } else {
+        this.setState({
+          user: res.data
+        });
+      }
+    });
+    console.log(this.state.user);
+  }
 
+  sendUserToHome(newuser) {
+    this.close(); //close modal component
+    this.setState({
+      user: newuser
+    });
+    console.log(this.state);
+  }
 
-    sendUserToHome(newuser){
-	this.close();//close modal component
-	this.setState({
-	    user: newuser
-	});
-	console.log(this.state);
-    }
+  isEmpty(obj) {
+    return Object.keys(obj).length === 0 && obj.constructor === Object;
+  }
 
-    isEmpty(obj){
-	return Object.keys(obj).length === 0 && obj.constructor === Object;
-    }
+  close() {
+    this.setState({ showModal: false });
+  }
 
-    close() {
-	this.setState({ showModal: false });
-    }
+  handleClick() {
+    this.setState({ showModal: true });
+  }
 
-    handleClick() {
-	this.setState({ showModal: true });
-    }
-
-    render() {
-	return (
+  render() {
+    return (
       <div id="site">
         <Router history={history}>
           <div id="site-content">
@@ -124,27 +122,29 @@ class App extends Component {
                   <NavItem disabled>
                     <p>En/Ch</p>
                   </NavItem>
-<<<<<<< HEAD
 
-            {this.isEmpty(this.state.user) ? <NavItem><p onClick={this.handleClick}>Log in</p></NavItem> : <AccountButton sendUserToHome={this.sendUserToHome} user={this.state.user}/>}
-
-=======
-                  <NavItem onClick={this.handleClick}>
-                    <p>Log in</p>
-                  </NavItem>
->>>>>>> In navigation bar, wrap NavItem inside LinkContainer
+                  {this.isEmpty(this.state.user) ? (
+                    <NavItem>
+                      <p onClick={this.handleClick}>Log in</p>
+                    </NavItem>
+                  ) : (
+                    <AccountButton
+                      sendUserToHome={this.sendUserToHome}
+                      user={this.state.user}
+                    />
+                  )}
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
-		<Route exact path="/" component={IndexPage} />
-		<Route path="/new-listing" component={NewListing} />
-		<Route path="/faq" component={Faq} />
-		<Route path="/commercial" component={Commercial} />
-		<Route path="/new-construction" component={NewConstructionList} />
-		<Route path="/info" component={Info} />
-		<Route path="/details/:id" component={Details} />
-		<Route path="/profile/:username" component={MyProfile} />
-		</div>
+            <Route exact path="/" component={IndexPage} />
+            <Route path="/new-listing" component={NewListing} />
+            <Route path="/faq" component={Faq} />
+            <Route path="/commercial" component={Commercial} />
+            <Route path="/new-construction" component={NewConstructionList} />
+            <Route path="/info" component={Info} />
+            <Route path="/details/:id" component={Details} />
+            <Route path="/profile/:username" component={MyProfile} />
+          </div>
         </Router>
 
         <LoginRegisForm
@@ -153,8 +153,8 @@ class App extends Component {
           onHide={this.close}
         />
 
-		<Footer />
-		</div>
+        <Footer />
+      </div>
     );
   }
 }
