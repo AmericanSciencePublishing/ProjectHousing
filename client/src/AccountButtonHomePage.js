0da035891ca5ba1c8e1b93729f558b3b62503895
Sihan Zhao <sihanzhao@dhcp81.merusouth125.iit.edu>
2017-12-01 09:42:13 -0600

    onMouseEnter open my account menu

--- client/src/AccountButtonHomePage.js
+++ client/src/AccountButtonHomePage.js
@@ -8,7 +8,18 @@ class AccountButtonHomePage extends React.Component{
 
     constructor(props){
 	super(props);
+	this.state = { isOpen : false };
+	this.handleOpen = this.handleOpen.bind(this);
+	this.handleClose = this.handleClose.bind(this);
 	this.handleSignOut=this.handleSignOut.bind(this);
+    }
+
+    handleOpen(){
+	this.setState({ isOpen : true });
+    }
+
+    handleClose(){
+	this.setState({ isOpen : false });
     }
 
     handleSignOut(event,eventKey){
@@ -25,15 +36,20 @@ class AccountButtonHomePage extends React.Component{
     render (){
         return(
 
-		<NavDropdown title="My Account" id="dropdown-custom-1">
-		<MenuItem header>Signed in as</MenuItem>
-		<MenuItem id="username" header><h4>{this.props.user.userName}</h4></MenuItem>
-		<MenuItem divider />
-		<MenuItem eventKey="1">My Profile</MenuItem>
-		<MenuItem eventKey="2">Saved Home</MenuItem>
-		<MenuItem eventKey="3" href="#">Setting</MenuItem>
-		<MenuItem divider />
-		<MenuItem eventKey="4" onSelect={this.handleSignOut}>Sign Out</MenuItem>
+		<NavDropdown
+		  onMouseEnter = { this.handleOpen }
+		  onMouseLeave = { this.handleClose }
+		  open={ this.state.isOpen }
+		  title="My Account"
+		  id="dropdown-custom-1">
+		  <MenuItem header>Signed in as</MenuItem>
+		  <MenuItem id="username" header><h4>{this.props.user.userName}</h4></MenuItem>
+		  <MenuItem divider />
+		  <MenuItem eventKey="1">My Profile</MenuItem>
+		  <MenuItem eventKey="2">Saved Home</MenuItem>
+		  <MenuItem eventKey="3" href="#">Setting</MenuItem>
+		  <MenuItem divider />
+		  <MenuItem eventKey="4" onSelect={this.handleSignOut}>Sign Out</MenuItem>
 		</NavDropdown>
 
 	);
