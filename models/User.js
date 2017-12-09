6763c98bab81214ca11c0dae1adbf200deaa2001
Yixian <puyixian@gmail.com>
2017-12-09 10:57:57 -0600

    User can save house to set "savedHouses" in user document

--- models/User.js
+++ models/User.js
@@ -4,102 +4,97 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 var userSchema = new Schema({
-    email: {
-	type: String,
-	unique: true,
-	trim: true
-    },
-    password: {
-	type: String
-    },
-    username:{
-	type:String
-    },
-    firstname:{
-	type:String
-    },
-    lastname:{
-	type:String
-    },
-    signupdate:{
-	type:String
-    },
-    signuptime:{
-        type:String
-    },
-    user_status:{
-	type: String
-    },
-    reset_pass_link:{
-	type: String
-    },
-    phone:{
-	type: String
-    },
-    address:{
-	type: String
-    },
-    bio:{
-	type: String,
-    },
-    saved_house:[String]
-	
+  email: {
+    type: String,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String
+  },
+  username: {
+    type: String
+  },
+  firstname: {
+    type: String
+  },
+  lastname: {
+    type: String
+  },
+  signupdate: {
+    type: String
+  },
+  signuptime: {
+    type: String
+  },
+  user_status: {
+    type: String
+  },
+  reset_pass_link: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
+  saved_house: [String]
 });
 
 //authenticate input against database
 //for the console log, 1:system err, 2:no such email, 3:login successfully, 4:wrong password with right email
-userSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-	if (err) {
-	    console.log('1');
-            return callback(err);
-	} else if (!user) {
-	    console.log('2');
-            var err = new Error('User not found.');
-            err.status = 401;
-	    console.log(err)
-            return callback(err);
-	}
-	bcrypt.compare(password, user.password, function (err, result) {
-            if (result === true) {
-		console.log('3');
-		return callback(null, user);
-            } else {
-		console.log('4');
-		return callback();
-		
-            }
-	})
+userSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email }).exec(function(err, user) {
+    if (err) {
+      console.log('1');
+      return callback(err);
+    } else if (!user) {
+      console.log('2');
+      var err = new Error('User not found.');
+      err.status = 401;
+      console.log(err);
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function(err, result) {
+      if (result === true) {
+        console.log('3');
+        return callback(null, user);
+      } else {
+        console.log('4');
+        return callback();
+      }
     });
-}
-
+  });
+};
 
 //hashing a password before saving it to the database
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', function(next) {
   var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash) {
+  bcrypt.hash(user.password, 10, function(err, hash) {
     if (err) {
       return next(err);
     }
     user.password = hash;
     next();
-  })
+  });
 });
 
 //please always use this toJSON() method to avoid sending hashpassword
 //or any other values that dont need to be send back to client.
 userSchema.set('toJSON', {
-    transform: function(doc, ret, options) {
-	delete ret.password;
-	delete ret.reset_pass_link;
-	delete ret.user_status;
-	delete ret.signuptime;
-        return ret;
-    }
+  transform: function(doc, ret, options) {
+    delete ret.password;
+    delete ret.reset_pass_link;
+    delete ret.user_status;
+    delete ret.signuptime;
+    return ret;
+  }
 });
-
 
 const User = mongoose.model('users', userSchema);
 
--- routes/save_house.js
+++ routes/save_house.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(req, res){
-  const id = req.body.id;
+var mongoose = require('mongoose');
+var User = require('../models/User');
+mongoose.connect(require('../config/keys').mLab);
 
-  console.log(`Saved house ID: ${id}`);
-  
-  res.end();
-})
+router.post('/', function(req, res) {
+  const houseID = req.body.id;
+
+  const userId = req.session.userId;
+
+  if (!userId) {
+    res.end();
+  }
+
+  User.findByIdAndUpdate(userId, {
+    $addToSet: { savedHouses: houseID }
+  }).then(doc => {
+    res.end();
+  });
+});
 
 module.exports = router;
