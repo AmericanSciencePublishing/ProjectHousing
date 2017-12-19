16fb4b311aea4a3b14c21acb63f48ec2f8f33085
Sihan Zhao <zhsh0616@gmail.com>
2017-12-19 09:34:02 -0600

    Map container and Map component preparation

---
+++ client/src/Map.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './MapComponent.css';
+
+
+export default class Map extends React.Component {
+    constructor(props) {
+    super(props);
+
+   
+    }
+    componentDidMount() {
+	console.log('map.js didmount check:',this.props);
+	this.loadMap();
+    }
+    componentDidUpdate(prevProps, prevState) {
+	console.log('map.js didupdate check:',this.props.google);
+	if (prevProps.google !== this.props.google) {
+	    this.loadMap();
+	}
+    }
+    
+    loadMap() {
+	if (this.props && this.props.google) {
+	    // google is available
+	    const {google} = this.props;
+	    const maps = google.maps;
+
+	    const mapRef = this.refs.map;
+	    const node = ReactDOM.findDOMNode(mapRef);
+
+	    let zoom = 10;
+	    let lat = 41.878876;
+	    let lng = -87.635915;
+	    const center = new maps.LatLng(lat, lng);
+	    const mapConfig = Object.assign({}, {
+		center: center,
+		zoom: zoom
+	    });
+	    console.log(mapConfig);
+	    this.map = new maps.Map(node, mapConfig);
+	   
+	}
+	// ...
+    }
+
+    renderChildren() {
+	const {children} = this.props;
+	
+	if (!children) return;
+
+	return React.Children.map(children, c => {
+	    if (!c) return;
+	    return React.cloneElement(c, {
+		map: this.map,
+		google: this.props.google
+	    });
+	});
+    }
+    
+    render() {
+	return (
+	    <div>
+	    <div ref='map' >
+              Loading map...
+	    </div>
+	    {this.renderChildren()}
+	    </div>
+	);
+    }
+}
--- client/src/MapContainer.js
+++ client/src/MapContainer.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import {} from 'react-bootstrap';
-import {GoogleApiWrapper} from 'google-maps-react';
+import {Map, GoogleApiWrapper} from 'google-maps-react';
+import './MapComponent.css';
+//import Map from './Map';
 
 export class MapContainer extends React.Component {
-  render() {
-    if (!this.props.loaded) {
-      return <div>Loading...</div>
+    render() {
+	if (!this.props.loaded) {
+	    return <div>Loading...</div>
+	}
+
+	return (
+	    <div className="MapContainerOriginal">
+	      <Map
+		google={this.props.google}
+		zoom={11}
+		initialCenter={{
+		    lat: 41.8863,
+		    lng: -87.7173
+		}}
+		>
+	      </Map>
+	    </div>
+	);
     }
-    return (
-      <div>Map will go here</div>
-    )
-  }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDVfVbaJ2245-ftBYAFMXHFTSCiInqI61k'
-})(MapContainer)
+    apiKey: 'AIzaSyDVfVbaJ2245-ftBYAFMXHFTSCiInqI61k'
+})(MapContainer);
