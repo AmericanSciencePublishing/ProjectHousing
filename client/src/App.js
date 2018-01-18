f7a86212fa01e4f3c85e22026fe4221dfb7071cb
Yixian <puyixian@gmail.com>
2018-01-18 10:05:35 -0600

    Commercial tab directs to a house-list?type=Commercial

--- client/src/App.js
+++ client/src/App.js
@@ -106,7 +106,7 @@ class App extends Component {
                       <p>New Listing</p>
                     </NavItem>
                   </LinkContainer>
-                  <LinkContainer to="/commercial">
+                  <LinkContainer to="/house-list?type=Commercial">
                     <NavItem>
                       <p>Commercial</p>
                     </NavItem>
@@ -149,7 +149,6 @@ class App extends Component {
               <Route exact path="/" component={IndexPage} />
               <Route path="/new-listing" component={NewListing} />
               <Route path="/faq" component={Faq} />
-              <Route path="/commercial" component={Commercial} />
               <Route path="/new-construction" component={NewConstructionList} />
               <Route path="/house-list" component={HouseList} />
               <Route path="/info" component={Info} />
