6160d8fdaa8600cd241c4cb2ab83d4c6607a5f9f
Yizhi Hong <yhong25@hawk.iit.edu>
2018-04-07 09:58:21 -0500

    Update google map Use new API

--- client/src/MapContainer.js
+++ client/src/MapContainer.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import {} from 'react-bootstrap';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import Thumbnail from './Thumbnail';
 import './MapComponent.css';
-import Marker from './MapMarker';
-//import Map from './Map';
 
-export class MapContainer extends React.Component {
-    render() {
-	if (!this.props.loaded) {
-	    return <div>Loading...</div>
+const convertPrice = (price) => {
+	price = price.split(',')
+	if (price.length === 2){
+		price = price[0] + "k"
+	}else if (price.length === 3){
+		price = price[0] + "m"
+	}else{
+		price = price[0] + "b"
 	}
-	const houseList = this.props.houseList || [];
-	return (
-	    <div className="MapContainerOriginal">
-	      <Map
-		google={this.props.google}
-		zoom={10}
-		initialCenter={{
-		    lat: 41.8863,
-		    lng: -87.7173
-		}}
-		>
-		{houseList.map(house => <Marker position={house.pos} key={house._id} />)}
-	      </Map>
-	    </div>
-	);
-    }
+	return price
 }
 
-export default GoogleApiWrapper({
-    apiKey: 'AIzaSyDVfVbaJ2245-ftBYAFMXHFTSCiInqI61k'
-})(MapContainer);
+class MapContainer extends React.Component {
+
+  render() {
+		const MyMapComponent = withGoogleMap((props) =>
+			<GoogleMap
+			defaultZoom={8}
+			defaultCenter={{ lat: 41.8863, lng: -87.7173 }}
+			>
+			{this.props.houseList.map(house => {console.log(house.pos);
+				return <Marker label={convertPrice(house.price)} position={house.pos} key={house._id} />})}
+			</GoogleMap>
+		)
+
+		const houseList = this.props.houseList || [];
+
+		console.log('HOUSE_LIST',houseList);
+		return (
+			<div className="MapContainerOriginal">
+				<MyMapComponent
+					containerElement={ <div style={{ width: `100%`, height: `100%`}} /> }
+          			mapElement={ <div style={{ height: `100%` }} /> }
+					houseList={houseList}
+				>
+				</MyMapComponent>
+			</div>
+		);
+  }
+}
+
+export default MapContainer;
--- client/src/MapMarker.js
+++ client/src/MapMarker.js
@@ -3,34 +3,33 @@ import PropTypes from 'prop-types';
 
 export default class MapMarker extends React.Component {
     render() {
-	return null;
+			return null;
     }
     
     componentWillUpdate(prevProps){
-	console.log('marker onmount');
+			console.log('marker onmount');
     }
     
     componentDidUpdate(prevProps) {
+    	console.log('marker onmount');
 	// component updated
-	if ((this.props.map !== prevProps.map) ||
-	    (this.props.position !== prevProps.position)) {
-            // The relevant props have changed
-	    this.renderMarker();
-	}
+			if ((this.props.map !== prevProps.map) ||
+			    (this.props.position !== prevProps.position)) {
+		            // The relevant props have changed
+			    this.renderMarker();
+			}
     }
     renderMarker() {
-	let {
-            map, google, position
-	} = this.props;
-	
-	let pos = position;
-	position = new google.maps.LatLng(pos.lat, pos.lng);
-	
-	const pref = {
-            map: map,
-            position: position
-	};
-	this.marker = new google.maps.Marker(pref);
+			let {map, google, position} = this.props;
+			
+			let pos = position;
+			position = new google.maps.LatLng(pos.lat, pos.lng);
+			
+			const pref = {
+				map: map,
+				position: position
+			};
+			this.marker = new google.maps.Marker(pref);
     }
 }
 
